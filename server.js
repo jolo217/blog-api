d568076bec0a870fe05f184a9f592f4f332a4247
jolo217 <joloxj@yahoo.com>
2017-08-31 23:02:41 -0500

    added travis

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const blogPostsRouter = require('./blogPostsRouter');
 const app = express();
 
+const BlogPostsRouter = require('./BlogPostsRouter');
 
 app.use(morgan('common'));
 
-// you need to import `blogPostsRouter` router and route
-// requests to HTTP requests to `/blog-posts` to `blogPostsRouter`
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
 app.use('/blog-posts', BlogPostsRouter);
+
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
+
